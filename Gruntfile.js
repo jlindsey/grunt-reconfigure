0352e00c3d5bf40a2f02767d205d590371d13644
Josh Lindsey <josh@core-apps.com>
2013-07-12 01:32:30 -0400

    Man it's late

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,7 +12,6 @@ module.exports = function(grunt) {
   grunt.initConfig({
     jshint: {
       options: {
-        reconfigureOverrides: {
         "laxcomma": true,
         "curly": true,
         "eqeqeq": true,
